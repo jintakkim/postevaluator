0909e9ca4c5e6e31fc7d364133bdf94975d72d63
jintakkim <mine407603@gmail.com>
2025-11-05 15:35:59 +0900

    chore: 핵심 용어에 맞게 주석 추가

--- src/main/java/com/jintakkim/evaluate/RecommendAlgorithm.java
+++ src/main/java/com/jintakkim/evaluate/RecommendAlgorithm.java
@@ -5,7 +5,7 @@ import com.jintakkim.evaluate.domain.PostFeature;
 @FunctionalInterface
 public interface RecommendAlgorithm {
     /**
-     * @return min 0.0 ~ max 1.0 사이의 수 추천도가 높을 수록 커진다.
+     * @return predictScore min 0.0 ~ max 1.0 사이의 수 추천도가 높을 수록 커진다.
      */
     double calculateScore(PostFeature postFeature);
 }
