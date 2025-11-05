d3080fd48b05b86794d2290a189356e8e7920652
jintakkim <mine407603@gmail.com>
2025-11-05 16:05:29 +0900

    feat: 알고리즘 평가 지표(mse) 구현

---
+++ src/main/java/com/jintakkim/evaluate/metric/AlgorithmMetric.java
@@ -0,0 +1,18 @@
+package com.jintakkim.evaluate.metric;
+
+import java.util.List;
+
+public interface AlgorithmMetric {
+    /**
+     * 알고리즘 평가시 사용된 메트릭 이름( 예: MSE)
+     */
+    String getName();
+
+    /**
+     * 비용(cost)을 계산하는 로직
+     * @param score 스코어
+     * @param scorePredict 알고리즘으로 부터 예측된 스코어, 스코어와 리스트 내에서의 인덱스가 일치해야한다.
+     * @return 비용
+     */
+    double calculateCost(List<Double> score, List<Double> scorePredict);
+}
---
+++ src/main/java/com/jintakkim/evaluate/metric/MSEMetric.java
@@ -0,0 +1,29 @@
+package com.jintakkim.evaluate.metric;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class MSEMetric implements AlgorithmMetric {
+    @Override
+    public String getName() {
+        return "MSE";
+    }
+
+    @Override
+    public double calculateCost(List<Double> score, List<Double> scorePredict) {
+        validateInput(score, scorePredict);
+        double sumSquaredError = 0.0;
+        for (int i = 0; i < score.size(); i++) {
+            double error = score.get(i) - scorePredict.get(i);
+            sumSquaredError += (error * error);
+        }
+        return sumSquaredError / score.size();
+    }
+
+    private void validateInput(List<Double> score, List<Double> scorePredict) {
+        if (scorePredict == null || score == null || score.size() != scorePredict.size())
+            throw new IllegalArgumentException("score과 scorePredict 리스트는 null이 아니어야 하며, 크기가 같아야 합니다.");
+    }
+}
