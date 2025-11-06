f11c36e33cb5fc2c383d4a048fc019d7de67759a
jintakkim <mine407603@gmail.com>
2025-11-06 16:52:08 +0900

    fix: core 패키지에서 metric패키지로의 의존성이 생기지 않게 LabeledPost를 리턴

--- src/main/java/com/jintakkim/postevaluator/core/infra/LabeledPostRepository.java
+++ src/main/java/com/jintakkim/postevaluator/core/infra/LabeledPostRepository.java
@@ -1,12 +1,11 @@
 package com.jintakkim.postevaluator.core.infra;
 
 import com.jintakkim.postevaluator.core.LabeledPost;
-import com.jintakkim.postevaluator.evaluator.EvaluatedPost;
 
 import java.util.List;
 
 public interface LabeledPostRepository {
     void save(LabeledPost post);
     List<Long> findUnlabeledFeatureIds();
-    List<EvaluatedPost> findAllEvaluatedPosts();
+    List<LabeledPost> findAll();
 }
