2647b87f8bba9600efb20cda2acefcb1b218c6e6
jintakkim <mine407603@gmail.com>
2025-11-06 15:51:39 +0900

    change: feature 대신 id를 저장하고 라벨링한 주체도 저장하도록 변경

--- src/main/java/com/jintakkim/postevaluator/core/LabeledPost.java
+++ src/main/java/com/jintakkim/postevaluator/core/LabeledPost.java
@@ -1,8 +1,9 @@
 package com.jintakkim.postevaluator.core;
 
 public record LabeledPost(
-        PostFeature feature,
+        long featureId,
         double score,
-        String reasoning
+        String reasoning,
+        String labeledModel
 ) {
 }
