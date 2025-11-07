53b91deeec8ce9c1e95cd50d6616b942b55d2610
jintakkim <mine407603@gmail.com>
2025-11-07 14:15:32 +0900

    feat: 초기에 데이터 베이스 스키마를 생성하도록 추가

--- src/main/java/com/jintakkim/postevaluator/core/infra/DbConfig.java
+++ src/main/java/com/jintakkim/postevaluator/core/infra/DbConfig.java
@@ -4,11 +4,16 @@ import com.jintakkim.postevaluator.core.LabeledPost;
 import com.jintakkim.postevaluator.core.PostFeature;
 import org.jdbi.v3.core.Jdbi;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 
 public class DbConfig {
-    private final Jdbi jdbi;
+    private static final String DB_FILE = "post_evaluator.db";
+    private static final String DIR = "user.home";
+    private static final String SCHEMA_FILE = "schema.sql";
 
+    public final Jdbi jdbi;
     public final LabeledPostRepository labeledPostRepository;
     public final PostFeatureRepository postFeatureRepository;
 
@@ -17,16 +22,30 @@ public class DbConfig {
         jdbi.registerRowMapper(PostFeature.class, new PostFeatureMapper());
         jdbi.registerRowMapper(LabeledPost.class, new LabeledPostMapper());
         this.jdbi = jdbi;
+        initializeDatabaseSchema();
         this.postFeatureRepository = new LocalFilePostFeatureRepository(jdbi);
         this.labeledPostRepository = new LocalFileLabeledPostRepository(jdbi);
     }
 
     /**
-     * home 위치에 저장
      * todo: 커스텀 환경 변수로으로 저장 위치 받을 수 있게 변경
      */
     private static String createDbUrl() {
-        String homeDir = System.getProperty("user.home");
-        return "jdbc:sqlite:" + Paths.get(homeDir + "post_evaluator.db");
+        String dir = System.getProperty(DIR);
+        return "jdbc:sqlite:" + Paths.get(dir + DB_FILE);
+    }
+
+    private void initializeDatabaseSchema() {
+        String schema = loadSqlFromResources(SCHEMA_FILE);
+        jdbi.useHandle(handle -> handle.createScript(schema).execute());
+    }
+
+    private String loadSqlFromResources(String fileName) {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            if (is == null) throw new IOException("sql 리소스를 찾을 수 없습니다: " + fileName);
+            return new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
