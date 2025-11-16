6686668624f9850ce62aaa9399a954f632a12e00
jintakkim <mine407603@gmail.com>
2025-11-16 14:53:48 +0900

    feat: 병렬 평가기 구현

--- src/main/java/com/jintakkim/postevaluator/config/EvaluatorConfig.java
+++ src/main/java/com/jintakkim/postevaluator/config/EvaluatorConfig.java
@@ -1,6 +1,7 @@
 package com.jintakkim.postevaluator.config;
 
 import com.jintakkim.postevaluator.DatasetManager;
+import com.jintakkim.postevaluator.evaluation.BulkEvaluator;
 import com.jintakkim.postevaluator.evaluation.Evaluator;
 import com.jintakkim.postevaluator.evaluation.SimpleEvaluator;
 import com.jintakkim.postevaluator.evaluation.metric.AlgorithmMetric;
@@ -9,12 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class EvaluatorConfig {
     public final Evaluator evaluator;
+    public final BulkEvaluator bulkEvaluator;
 
     public EvaluatorConfig(
             AlgorithmMetric algorithmMetric,
             DatasetManager datasetManager
     ) {
         this.evaluator = new SimpleEvaluator(datasetManager.getEvaluationPosts(), algorithmMetric);
+        this.bulkEvaluator = new BulkEvaluator(evaluator);
     }
 
 
--- src/main/java/com/jintakkim/postevaluator/evaluation/Evaluator.java
+++ src/main/java/com/jintakkim/postevaluator/evaluation/Evaluator.java
@@ -4,6 +4,7 @@ import com.jintakkim.postevaluator.core.RecommendAlgorithm;
 
 /**
  * 게시글을 평가하는 평가기
+ * thread-safe
  */
 public interface Evaluator {
     EvaluateResult evaluate(RecommendAlgorithm recommendAlgorithm);
