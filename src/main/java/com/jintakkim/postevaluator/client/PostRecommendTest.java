5f3becc49fde3b2723b0927a39db4e64c7ea3b8f
jintakkim <mine407603@gmail.com>
2025-11-16 14:54:38 +0900

    feat: 테스트 사용자 인터페이스 구현

---
+++ src/main/java/com/jintakkim/postevaluator/client/PostRecommendTest.java
@@ -0,0 +1,39 @@
+package com.jintakkim.postevaluator.client;
+
+import com.jintakkim.postevaluator.config.*;
+import com.jintakkim.postevaluator.core.RecommendAlgorithm;
+import com.jintakkim.postevaluator.evaluation.EvaluateResult;
+import com.jintakkim.postevaluator.search.GridSearcher;
+import com.jintakkim.postevaluator.search.ParametricRecommendAlgorithm;
+import com.jintakkim.postevaluator.search.SearchResult;
+import com.jintakkim.postevaluator.search.param.CombinationProvider;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class PostRecommendTest {
+    private final ApplicationConfig appConfig;
+
+    public PostRecommendTest(
+            ApplicationConfig appConfig
+    ) {
+        this.appConfig = appConfig;
+    }
+
+    public EvaluateResult simpleTest(RecommendAlgorithm recommendAlgorithm) {
+        return appConfig.evaluatorConfig.evaluator.evaluate(recommendAlgorithm);
+    }
+
+    public List<EvaluateResult> bulkTest(List<RecommendAlgorithm> recommendAlgorithms) {
+        return appConfig.evaluatorConfig.bulkEvaluator.evaluate(recommendAlgorithms);
+    }
+
+    public SearchResult gridSearchTest(
+            CombinationProvider combinationProvider,
+            ParametricRecommendAlgorithm parametricRecommendAlgorithm
+    ) {
+        GridSearcher gridSearcher = new GridSearcher(appConfig.evaluatorConfig.evaluator, combinationProvider);
+        return gridSearcher.search(parametricRecommendAlgorithm);
+    }
+}
