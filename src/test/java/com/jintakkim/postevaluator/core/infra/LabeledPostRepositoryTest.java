75da8ef44465485e6aa50728edc1386cde5944bf
jintakkim <mine407603@gmail.com>
2025-11-19 17:30:14 +0900

    change: Post 변경에 따른 테스트 수정

--- src/test/java/com/jintakkim/postevaluator/core/infra/LabeledPostRepositoryTest.java
+++ src/test/java/com/jintakkim/postevaluator/core/infra/LabeledPostRepositoryTest.java
@@ -1,54 +1,40 @@
 package com.jintakkim.postevaluator.core.infra;
 
 import com.jintakkim.postevaluator.core.LabeledPost;
-import com.jintakkim.postevaluator.core.PostFeature;
+import com.jintakkim.postevaluator.core.Post;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
 
 public class LabeledPostRepositoryTest extends LocalFileDbIntegrationTest {
+
     @Test
     @DisplayName("LabeledPost 저장 후, findAll()로 조회 시 정확히 일치해야 한다")
     void saveShouldBeRetrievedByFindAll() {
-        PostFeature postFeature = postFeatureRepository.save(createPostFeature());
-        LabeledPost savedLabeledPost = labeledPostRepository.save(createLabeledPost(postFeature.id()));
-        Assertions.assertThat(labeledPostRepository.findAll())
-                .hasSize(1)
-                .containsExactly(savedLabeledPost);
+        rollbackTest(() -> {
+            Post post = postRepository.save(createPost());
+            LabeledPost savedLabeledPost = labeledPostRepository.save(createLabeledPost(post.id()));
+            Assertions.assertThat(labeledPostRepository.findAll())
+                    .hasSize(1)
+                    .containsExactly(savedLabeledPost);
+        });
     }
 
     @Test
-    @DisplayName("래이블링 되지 않은 featureId를 반환한다")
+    @DisplayName("래이블링 되지 않은 postId를 반환한다")
     void findUnlabeledFeatureIdsShouldReturnOnlyIdsWithNoLabels() {
-        PostFeature postFeature_1 = postFeatureRepository.save(createPostFeature());
-        PostFeature postFeature_2 = postFeatureRepository.save(createPostFeature());
-        PostFeature postFeature_3 = postFeatureRepository.save(createPostFeature());
+        rollbackTest(() -> {
+            Post post_1 = postRepository.save(createPost());
+            Post post_2 = postRepository.save(createPost());
+            Post post_3 = postRepository.save(createPost());
 
-        labeledPostRepository.save(createLabeledPost(postFeature_1.id()));
-
-        List<Long> unlabeledPost =labeledPostRepository.findUnlabeledFeatureIds();
-        Assertions.assertThat(unlabeledPost)
-                .hasSize(2)
-                .containsExactlyInAnyOrder(postFeature_2.id(), postFeature_3.id());
-    }
-
-    private PostFeature createPostFeature() {
-        return new PostFeature(
-                0L,
-                0L,
-                0L,
-                0L,
-                "content",
-                OffsetDateTime.of(LocalDateTime.of(2025, 11, 7, 0, 0), ZoneOffset.UTC).toString()
-        );
-    }
-
-    private LabeledPost createLabeledPost(Long featureId) {
-        return new LabeledPost(featureId, 1.0, "good");
+            labeledPostRepository.save(createLabeledPost(post_1.id()));
+            List<Long> unlabeledPost =labeledPostRepository.findUnlabeledPostIds();
+            Assertions.assertThat(unlabeledPost)
+                    .hasSize(2)
+                    .containsExactlyInAnyOrder(post_2.id(), post_3.id());
+        });
     }
 }
--- src/test/java/com/jintakkim/postevaluator/core/infra/LocalFileDbIntegrationTest.java
+++ src/test/java/com/jintakkim/postevaluator/core/infra/LocalFileDbIntegrationTest.java
@@ -1,11 +1,19 @@
 package com.jintakkim.postevaluator.core.infra;
 
 import com.jintakkim.postevaluator.config.DbConfig;
+import com.jintakkim.postevaluator.core.LabeledPost;
+import com.jintakkim.postevaluator.core.Post;
+import com.jintakkim.postevaluator.feature.Feature;
+import com.jintakkim.postevaluator.feature.FeatureProvider;
+import com.jintakkim.postevaluator.fixture.FeatureFixture;
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 실제 로컬 파일에 저장이 되는 테스트
@@ -13,27 +21,49 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 @Slf4j
 @Execution(ExecutionMode.SAME_THREAD) //기본값이지만 db 데이터에 의해 테스트간 격리가 깨질 수 있으므로 명시
 public abstract class LocalFileDbIntegrationTest {
+    protected static final List<Feature> TEST_FEATURES = List.of(FeatureFixture.COMMENT_COUNT, FeatureFixture.LIKE_COUNT);
+
     protected static LabeledPostRepository labeledPostRepository;
-    protected static PostFeatureRepository postFeatureRepository;
+    protected static PostRepository postRepository;
     protected static Jdbi jdbi;
 
     @BeforeAll
     static void initialize() {
-        log.info("db 설정");
-        DbConfig dbConfig = new DbConfig("test.db");
+        DbConfig dbConfig = new DbConfig("test.db", featureProvider());
         jdbi = dbConfig.jdbi;
         labeledPostRepository = dbConfig.labeledPostRepository;
-        postFeatureRepository = dbConfig.postFeatureRepository;
+        postRepository = dbConfig.postRepository;
     }
 
-    @AfterAll
-    static void clearAll() {
-        log.info("테이블의 모든 데이터 삭제");
-        jdbi.useHandle(handle -> {
-            handle.execute("DELETE FROM labeled_post");
-            handle.execute("DELETE FROM post_feature");
-            //id 시퀀스를 다시 처음 부터 시작
-            handle.execute("DELETE FROM sqlite_sequence WHERE name IN ('labeled_post', 'post_feature')");
+    static void rollbackTest(Runnable runnable) {
+        jdbi.useTransaction(handle -> {
+            runnable.run();
+            handle.rollback();
         });
+    }
+
+    static FeatureProvider featureProvider() {
+        return new FeatureProvider() {
+            @Override
+            public List<String> getFeatureNames() {
+                return TEST_FEATURES.stream().map(Feature::getName).toList();
+            }
+
+            @Override
+            public List<Feature> getFeatures() {
+                return TEST_FEATURES;
+            }
+        };
+    }
+
+    static Post createPost() {
+        return new Post(null, Map.of(
+                FeatureFixture.COMMENT_COUNT.getName(), 10L,
+                FeatureFixture.LIKE_COUNT.getName(), 100L
+        ));
+    }
+
+    static LabeledPost createLabeledPost(Long featureId) {
+        return new LabeledPost(featureId, 1.0, "reason??");
     }
 }
