8870d9cbb6dfac3b8eabc35d18c2ad50855aba82
jintakkim <mine407603@gmail.com>
2025-11-21 17:32:43 +0900

    change: FeatureAccesor로 명칭 변경

--- src/main/java/com/jintakkim/postevaluator/search/ParametricRecommendAlgorithm.java
+++ src/main/java/com/jintakkim/postevaluator/search/ParametricRecommendAlgorithm.java
@@ -1,11 +1,12 @@
 package com.jintakkim.postevaluator.search;
 
 import com.jintakkim.postevaluator.core.Post;
+import com.jintakkim.postevaluator.evaluation.FeatureAccessor;
 import com.jintakkim.postevaluator.search.param.Combination;
 
 public interface ParametricRecommendAlgorithm {
     /**
      * @return predictScore min 0.0 ~ max 1.0 사이의 수 추천도가 높을 수록 커진다.
      */
-    double calculateScore(Post post, Combination combination);
+    double calculateScore(FeatureAccessor featureAccessor, Combination combination);
 }
