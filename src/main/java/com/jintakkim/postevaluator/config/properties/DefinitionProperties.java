ce13c608fa2add30c4476644373d57e82201db2a
jintakkim <mine407603@gmail.com>
2025-11-23 17:11:31 +0900

    refactor: definitionProperties를 record로 변경

--- src/main/java/com/jintakkim/postevaluator/config/properties/DefinitionProperties.java
+++ src/main/java/com/jintakkim/postevaluator/config/properties/DefinitionProperties.java
@@ -3,7 +3,4 @@ package com.jintakkim.postevaluator.config.properties;
 import com.jintakkim.postevaluator.PostDefinition;
 import com.jintakkim.postevaluator.UserDefinition;
 
-public class DefinitionProperties {
-    public UserDefinition userDefinition;
-    public PostDefinition postDefinition;
-}
+public record DefinitionProperties (UserDefinition userDefinition, PostDefinition postDefinition) {}
