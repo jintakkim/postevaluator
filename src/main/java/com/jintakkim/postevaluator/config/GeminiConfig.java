11a243900f0111835d8e747536d0a22c804f7346
jintakkim <mine407603@gmail.com>
2025-11-24 21:09:48 +0900

    feat: gemini환경 변수 설정 안되어있을때 방어 코드 추가

--- src/main/java/com/jintakkim/postevaluator/config/GeminiConfig.java
+++ src/main/java/com/jintakkim/postevaluator/config/GeminiConfig.java
@@ -6,7 +6,13 @@ public class GeminiConfig {
     public final Client client;
 
     public GeminiConfig() {
+        validateEnvSetting();
         this.client = new Client();
+    }
+
+    private void validateEnvSetting() {
+        String apiKey = System.getenv("GOOGLE_API_KEY");
+        if(apiKey == null) throw new RuntimeException("GOOGLE_API_KEY 환경 변수 설정이 되어있지 않습니다.");
     }
 }
 
